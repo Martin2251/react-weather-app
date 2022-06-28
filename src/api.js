278b6c2d6aebb4bcd804d1a0f688678ffa681f75
Martin Lowe <59805940+Martin2251@users.noreply.github.com>
2022-06-28 08:03:30 +0100

    on change value on

---
+++ src/api.js
@@ -0,0 +1,11 @@
+export const geoApiOptions = {
+    method: 'GET',
+    headers: {
+        'X-RapidAPI-Key': 'e0db8e15edmshd2ee394bb7399e8p11f48fjsn41b4ece282b9',
+        'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com'
+    }
+};
+
+export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo";
+
+
